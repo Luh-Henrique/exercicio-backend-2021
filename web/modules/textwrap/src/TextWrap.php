27994eee4ae41643c8345ca520bfa3d525e61786
Luh-Henrique <l.henrique.silva1010@gmail.com>
2021-09-01 12:06:58 -0300

    Added final logic for string wrapping and passing to final array

--- web/modules/textwrap/src/TextWrap.php
+++ web/modules/textwrap/src/TextWrap.php
@@ -50,5 +50,64 @@ class TextWrap implements TextWrapInterface {
       $array_ini[$j] = $array_ini[$j].$text[$i];
       }
     }
+
+    //Resetando variavel
+    $j = 0;
+    $k = 0;
+    $array_final[0] = '';
+
+    //Laço para manipulação final da string
+    for($i=0;$i<count($array_ini);$i++){
+  
+      //Primeiro verifica se o tamanho do que já está no array final somado com a palavra 
+      //que estamos analisando é menor que o tamanho especificado
+      if((mb_strlen($array_final[$k])+mb_strlen($array_ini[$i]))<$length){
+
+        //Caso sim e não tenha nada na posição do array final ainda copia a palavra inteira
+        if(empty($array_final[$k])){
+          $array_final[$k] = $array_final[$k].$array_ini[$i];
+        }
+
+        //Caso ja tenha algo ele coloca um espaço entre as palavras
+        else{
+          $array_final[$k] = $array_final[$k].' '.$array_ini[$i];
+        }
+
+      }
+
+      //Caso a palavra que está no array final somada a palavra analisada exceda o tamanho especificado
+      else{
+
+        //Muda a posição do vetor e se inicia a posição como vazia para não dar erro
+        $k++;
+        $array_final[$k] = '';
+
+        //Caso a palavra esteja dentro do limite especificado ela é escrita por inteiro
+        if(mb_strlen($array_ini[$i])<$length){
+          $array_final[$k] = $array_ini[$i];
+        }
+
+        //Caso negativo se copia caractere por caractere obedecendo o limite de caracteres
+        else{
+
+          //Variavel auxiliar para verificar se o atingiu o limite da linha para este caso
+          $n=0;
+          for($j=0;$j<mb_strlen($array_ini[$i]);$j++){
+            $n++;
+            $array_final[$k] = $array_final[$k].$array_ini[$i][$j];
+
+            //Caso o limite de caracteres seja atingido se troca a posição do vetor
+            if($n>$length){
+            $k++;
+            $n=0;
+            $array_final[$k]='';;
+            }
+          }
+        }
+      }
+    }
+
+    //Retorna o array final com as palavras quebradas
+    return $array_final;
   }
 }
